3130e972584673231284712911d767d3fb584caa
ubuntutest10@gmail.com <MrLostInTheInternet>
2022-09-02 15:26:08 +0200

    Update 2/9

--- Project Fluidsim/fluid.py
+++ Project Fluidsim/fluid.py
@@ -1,52 +1,57 @@
 #!/usr/bin/env python3
 
 import argparse
-import errno
-from os import strerror
+
 import sys
 import string
 import textwrap
+ 
+def insert_correct_stroke():
+    stroke = input("Error. Insert the correct stroke: ")
+    return stroke
 
-def controllo_stroke(stroke):                                                                   #controllo della corsa del pistone, deve essere in formato 'lettera' + '+ o -'
-    ok = False
-    while ok == False:                                                                          #variabile di controllo booleana, se Falsa allora la corsa non e' definita correttamente
-        try:
-            if stroke[0] in string.ascii_lowercase or stroke[0] in string.ascii_uppercase:      #controllo che ci sia una lettera e non un simbolo nel nome della corsa
-                
-                if stroke[1] == '+' or stroke[1] == '-':                                        #se ho una lettera prima, controllo che ci sia il '+' o il '-'
-                    ok = True
-                    break
 
-                else:
-                    ok = False                                                                  #corsa errata, ritorno ok Falso
-                    break
-
-            elif '/' in stroke[0]:                                                              #controllo se il comando di 'termina sequenza' e' stato inserito
-                ok = True
-                print("The sequence is terminated.\n")
-            
-            else:
-                print("A letter must be the name of the actuator")
-        
-        except len(stroke) > 2:                                                                 #una specie di limite buffer cosi non andiamo oltre la lunghezza di due dati inseriti
+def check_stroke(stroke):                                                                  
+    correct_stroke = False
+    while not correct_stroke:                                                                          
+        if len(stroke) > 2:                                                                 
             print("Too many arguments. ")
+            correct_stroke = False
             break
 
-    return ok
+        elif stroke[0] in string.ascii_lowercase or stroke[0] in string.ascii_uppercase:      
+            
+            if stroke[1] == '+' or stroke[1] == '-':                                       
+                correct_stroke = True
 
-def inserire_stroke(sequenza):
-    stroke = input("Inserire corsa: ")
-    while True:
-        ok = controllo_stroke(stroke)
-        if ok == False:
-            new_stroke = input("Inserire nuovamente corsa: ")
-            stroke = new_stroke
-            True
+            else:
+                correct_stroke = False                                                                  
+                
+
+        elif '/' in stroke[0]:                                                             
+            correct_stroke = True
+            print("The sequence is terminated.\n")
+        
         else:
-            False
-            break
-    sequenza.append(stroke)
-    '''with open("sequenza.txt","w") as f:
+            print("A letter must be the name of the actuator")
+        
+    return correct_stroke
+
+def insert_stroke(sequence):
+    stroke = input("Insert stroke: ")
+    continue_insert = False
+    while not continue_insert:
+        correct_stroke = check_stroke(stroke)
+        if correct_stroke != True:
+            if '/' not in stroke:
+                stroke = insert_correct_stroke()
+            else:
+                print("We can proceed with the analysis")
+        else:
+            continue_insert = True
+
+    sequence.append(stroke)
+    '''with open("sequence.txt","w") as f:
         try:
             if '/' not in stroke[0]:
                 print(stroke)
@@ -59,7 +64,7 @@ def inserire_stroke(sequenza):
                 f.close()
         except IOError as e:
             print(e)'''
-    return stroke, sequenza
+    return stroke, sequence
 
 class FluidPy:
     def __init__(self, args):
@@ -69,20 +74,23 @@ class FluidPy:
             self.loop()
         else:
             self.normal()
+    
+    #def sequence(self, sequence, args):
 
     def normal(self):
-        sequenza = []
+        sequence = []
         try:
             while True:
-                stroke, sequenza = inserire_stroke(sequenza)
+                stroke, sequence = insert_stroke(sequence)
                 if '/' in stroke:
-                    del sequenza[-1]
+                    del sequence[-1]
                     break
         except KeyboardInterrupt:
             print("User terminated the sequence.\n")
             sys.exit()
-        print(sequenza)
-            
+        print(sequence)
+        #check_sequence()
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
